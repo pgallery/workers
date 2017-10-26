2561899311d3c61e7b6114a1120525168fa2cbd6
Ruzhentsev Alexandr <webcode@pgallery.ru>
2017-10-26 16:56:08 +0300

    Modify

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -77,7 +77,7 @@ if [ ! -f /var/www/html/install.lock ]; then
         -e "s/DB_HOST=127.0.0.1/DB_HOST=${DB_HOST}/g" \
         -e "s/DB_PORT=3306/DB_PORT=${DB_PORT}/g" \
         -e "s/DB_DATABASE=homestead/DB_DATABASE=${DB_NAME}/g" \
-        -e "s/DB_USERNAME=homestead/DB_USERNAME=${DB_USER}/g" \
+        -e "s/DB_USERNAME=homestead/DB_USERNAME=${DB_USERNAME}/g" \
         -e "s/DB_PASSWORD=secret/DB_PASSWORD=${DB_PASSWORD}/g" \
         -e "s/CACHE_DRIVER=file/CACHE_DRIVER=${CACHE_DRIVER}/g" \
         -e "s/SESSION_DRIVER=file/SESSION_DRIVER=${SESSION_DRIVER}/g" \
